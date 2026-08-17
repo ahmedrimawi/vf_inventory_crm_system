42b79115b52dd93141d7cb73ddcaf4cbe176590e
Ahmed Atiea Al Rimawi <ahmedatieaalrimawi@Mac.lan>
2026-08-17 16:12:55 +0300

    Update flutter files

--- vf_inventory_crm_fe/lib/core/constants/api_constants.dart
+++ vf_inventory_crm_fe/lib/core/constants/api_constants.dart
@@ -1,6 +1,7 @@
 class ApiConstants {
   static const String baseUrl =
-      'https://vf-inventory-crm-system.onrender.com/api';
+      // 'https://vf-inventory-crm-system.onrender.com/api';
+      'http://127.0.0.1:8000/api/';
 
   static const String login = '/auth/login';
 }
